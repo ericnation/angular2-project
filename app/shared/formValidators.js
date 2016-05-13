9d6b1a428d87d8f9aae2edf7f9542edba7c4137d
Eric Nation <eric.nation@gmail.com>
2016-05-12 18:03:30 -0700

    generated files

--- app/shared/formValidators.js
+++ app/shared/formValidators.js
@@ -1,22 +1,13 @@
-System.register([], function(exports_1, context_1) {
-    "use strict";
-    var __moduleName = context_1 && context_1.id;
-    var generalFormValidators;
-    return {
-        setters:[],
-        execute: function() {
-            generalFormValidators = (function () {
-                function generalFormValidators() {
-                }
-                generalFormValidators.email = function (control) {
-                    var regEx = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
-                    var valid = regEx.test(control.value);
-                    return valid ? null : { email: true };
-                };
-                return generalFormValidators;
-            }());
-            exports_1("generalFormValidators", generalFormValidators);
-        }
+"use strict";
+var generalFormValidators = (function () {
+    function generalFormValidators() {
     }
-});
+    generalFormValidators.email = function (control) {
+        var regEx = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
+        var valid = regEx.test(control.value);
+        return valid ? null : { email: true };
+    };
+    return generalFormValidators;
+}());
+exports.generalFormValidators = generalFormValidators;
 //# sourceMappingURL=formValidators.js.map
